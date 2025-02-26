0b2c071b9c8c16b61026173ff6e55a9f3675dc70
waltteriwe <walttewe@metropolia.fi>
2025-02-26 11:27:06 +0200

    Implement LoginForm component with validation and add placeholder RegisterForm component

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,8 +1,64 @@
-import React from 'react';
-import {Text} from 'react-native';
+import {Button, Card, Input} from '@rneui/base';
+import {Controller, useForm} from 'react-hook-form';
+import {Credentials} from '../types/LocalTypes';
+import {useUserContext} from '../hooks/ContextHooks';
 
 const LoginForm = () => {
-  return <Text>LoginForm</Text>;
+  const {handleLogin} = useUserContext();
+  const initValues: Credentials = {username: '', password: ''};
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: initValues,
+  });
+
+  const doLogin = async (inputs: Credentials) => {
+    handleLogin(inputs);
+  };
+
+  return (
+    <Card>
+      <Controller
+        control={control}
+        rules={{
+          required: {value: true, message: 'is required'},
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            placeholder="Username"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            autoCapitalize="none"
+            errorMessage={errors.username?.message}
+          />
+        )}
+        name="username"
+      />
+
+      <Controller
+        control={control}
+        rules={{
+          maxLength: 100,
+          required: {value: true, message: 'is required'},
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            placeholder="password"
+            secureTextEntry
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            errorMessage={errors.password?.message}
+          />
+        )}
+        name="password"
+      />
+      <Button title="Login" onPress={handleSubmit(doLogin)} />
+    </Card>
+  );
 };
 
 export default LoginForm;
